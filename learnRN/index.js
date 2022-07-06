dd8a3c1c3b9f73b16c966771c9e1f49d224c0739
jwAndroid <cjd9408er@naver.com>
2022-07-06 13:50:47 +0900

    Update: App.js

--- learnRN/index.js
+++ learnRN/index.js
@@ -7,3 +7,6 @@ import App from './App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
+
+// 리액트 네이티브는 index.js 에서 시작한다.
+// registerComponent 메서드로 화면에 컴포넌트를 보여줄 수 있다.
---
+++ learnRN/src/components/Greeting.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+const Greeting = () => <Text style={{color: 'white'}}>Hello Wolrd!!</Text>;
+
+export default Greeting;
