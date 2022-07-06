7d6c5f4b6f1a45ad3e7dd9ee8afdbe1c49b92dc7
jwAndroid <cjd9408er@naver.com>
2022-07-06 14:51:52 +0900

    Create: TodoApp

---
+++ TodoApp/App.js
@@ -0,0 +1,16 @@
+/**
+ * @format
+ */
+
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Hello TodoApp </Text>
+    </View>
+  );
+};
+
+export default App();
