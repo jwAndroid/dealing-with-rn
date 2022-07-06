926b4e9648b910e3faa60b732f8fcb045b811990
jwAndroid <cjd9408er@naver.com>
2022-07-06 15:21:19 +0900

    Setup Podfile

--- TodoApp/App.js
+++ TodoApp/App.js
@@ -3,14 +3,22 @@
  */
 
 import React from 'react';
-import {Text, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 const App = () => {
   return (
-    <View>
-      <Text>Hello TodoApp </Text>
+    <View style={styles.container}>
+      <Text>Hello todoApp</Text>
     </View>
   );
 };
 
-export default App();
+export default App;
