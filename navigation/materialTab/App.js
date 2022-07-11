bc379da342d15a1ea329d789fe550ec5e295e28d
jwAndroid <cjd9408er@naver.com>
2022-07-11 19:00:17 +0900

    Update: material-tab-navigation

--- navigation/materialTab/App.js
+++ navigation/materialTab/App.js
@@ -3,9 +3,48 @@
  */
 
 import React from 'react';
+import {
+  getFocusedRouteNameFromRoute,
+  NavigationContainer,
+} from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import BottomPager from './screens/BottomPager';
+// import TopPager from './screens/TopPager';
+import DetailScreen from './screens/DetailScreen';
+
+const Stack = createNativeStackNavigator();
+
+const getHeaderTitle = route => {
+  const routeName = getFocusedRouteNameFromRoute(route) ?? 'Home';
+
+  const nameMap = {
+    Home: '홈',
+    Search: '검색',
+    Notification: '알림',
+    Message: '메세지',
+  };
+
+  return nameMap[routeName];
+};
 
 const App = () => {
-  return <></>;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        {/* <Stack.Screen name="Top" component={TopPager} /> */}
+        <Stack.Screen
+          name="Bottom"
+          component={BottomPager}
+          options={({ route }) => ({
+            title: getHeaderTitle(route),
+          })}
+        />
+
+        <Stack.Screen name="Detail" component={DetailScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 
 export default App;
