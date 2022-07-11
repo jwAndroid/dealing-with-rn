33a78c4c17821eb4c7b664fe6fe3b45572a13c45
jwAndroid <cjd9408er@naver.com>
2022-07-11 11:28:05 +0900

    Create: Drawer-navigation

---
+++ drawer/App.js
@@ -0,0 +1,16 @@
+/**
+ * @format
+ */
+
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>hello world</Text>
+    </View>
+  );
+};
+
+export default App;
