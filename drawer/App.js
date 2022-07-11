c150dbfbe51d785127b282470b6d1d9fb69000c7
jwAndroid <cjd9408er@naver.com>
2022-07-11 12:53:58 +0900

    Update: Drawer-navigation

--- drawer/App.js
+++ drawer/App.js
@@ -1,15 +1,46 @@
 /**
  * @format
  */
-
 import React from 'react';
-import {Text, View} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { View, Text, Button } from 'react-native';
 
-const App = () => {
+function HomeScreen({ navigation }) {
   return (
     <View>
-      <Text>hello world</Text>
+      <Text>Home</Text>
+      <Button title="Drawer 열기" onPress={() => navigation.openDrawer()} />
+      <Button
+        title="Setting 열기"
+        onPress={() => navigation.navigate('Setting')}
+      />
     </View>
+  );
+}
+
+function SettingScreen({ navigation }) {
+  return (
+    <View>
+      <Text>Setting</Text>
+      <Button title="뒤로가기" onPress={() => navigation.goBack()} />
+    </View>
+  );
+}
+
+const App = () => {
+  const Drawer = createDrawerNavigator();
+
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator
+        initialRouteName="Home"
+        drawerPosition="left"
+        backBehavior="history">
+        <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen name="Setting" component={SettingScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 };
 
