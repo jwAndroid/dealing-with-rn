5bdd0d216366dd617e524f07685627988f01cd74
jwAndroid <cjd9408er@naver.com>
2022-07-13 23:08:09 +0900

    Create: component

---
+++ DayLog/src/components/FloatingWriteButton.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+const styles = StyleSheet.create({
+  wrapper: {},
+});
+
+const FloatingWriteButton = () => {
+  return <View></View>;
+};
+
+export default FloatingWriteButton;
