8b6b9dbb9124859fd3aab7b3065e78ebb0aab911
jwAndroid <cjd9408er@naver.com>
2022-07-20 20:18:19 +0900

    Add: SearchHeader

---
+++ DayLog/src/components/SearchHeader.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  block: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  input: {
+    flex: 1,
+  },
+  button: {
+    marginLeft: 8,
+  },
+});
+
+const SearchHeader = () => {
+  return <></>;
+};
+
+export default SearchHeader;
