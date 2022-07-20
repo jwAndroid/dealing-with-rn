e0c338d11a5eb159ca8fac304605b33e3f9f8d51
jwAndroid <cjd9408er@naver.com>
2022-07-21 00:21:19 +0900

    Add : EmptySearchResult

--- DayLog/src/screens/SearchScreen.js
+++ DayLog/src/screens/SearchScreen.js
@@ -4,6 +4,7 @@ import { StyleSheet, View } from 'react-native';
 import SearchContext from '../context/SearchContext';
 import LogContext from '../context/LogContext';
 import FeedList from '../components/FeedList';
+import EmptySearchResult from '../components/EmptySearchResult';
 
 const styles = StyleSheet.create({
   block: { flex: 1 },
@@ -20,6 +21,14 @@ const SearchScreen = ({ navigation }) => {
           [log.title, log.body].some(text => text.includes(keyword)),
         );
   }, [keyword, logs]);
+
+  if (keyword === '') {
+    return <EmptySearchResult type="EMPTY_KEYWORD" />;
+  }
+
+  if (filiterdList.length === 0) {
+    return <EmptySearchResult type="NOT_FOUND" />;
+  }
 
   return (
     <View style={styles.block}>
