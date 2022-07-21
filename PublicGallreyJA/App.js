69ebf6ffe90d1541e1d11450b0dd3ea65f2939d6
jwAndroid <cjd9408er@naver.com>
2022-07-21 19:31:46 +0900

    Add : Project

---
+++ PublicGallreyJA/App.js
@@ -0,0 +1,18 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const App = () => {
+  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <Text>Hello World</Text>
+  </View>;
+};
+
+export default App;
