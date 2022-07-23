4cc6a9cf17d64341d1db1aae7acc48f9c0deefd3
jwAndroid <cjd9408er@naver.com>
2022-07-23 18:19:45 +0900

    Update read collection

--- SetupFirebase/App.js
+++ SetupFirebase/App.js
@@ -2,15 +2,16 @@
  * @format
  */
 
-import React, { memo, useCallback } from 'react';
+import React, { memo, useCallback, useEffect } from 'react';
 import { Text, TouchableHighlight, View } from 'react-native';
-import { collection, addDoc } from 'firebase/firestore';
+import { collection, addDoc, doc, getDoc, getDocs } from 'firebase/firestore';
 
 import { db } from './src/config';
+import { async } from '@firebase/util';
 
 const App = () => {
-  const add = useCallback(async () => {
-    const ref = 'user/items/1';
+  const onAdd = useCallback(async () => {
+    const ref = 'cities/SF/1';
 
     const obj = {
       one: 'one',
@@ -20,10 +21,22 @@ const App = () => {
     await addDoc(collection(db, ref), obj);
   }, []);
 
+  const onRead = useCallback(async () => {
+    const ref = collection(db, 'cities/SF/1');
+
+    const snapshot = await getDocs(ref);
+
+    snapshot.forEach(item => console.log(item.data()));
+  }, []);
+
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <TouchableHighlight onPress={add}>
+      <TouchableHighlight onPress={onAdd}>
         <Text style={{ fontSize: 50, color: 'black' }}>+</Text>
+      </TouchableHighlight>
+
+      <TouchableHighlight onPress={onRead}>
+        <Text style={{ fontSize: 50, color: 'black' }}>get doc</Text>
       </TouchableHighlight>
     </View>
   );
