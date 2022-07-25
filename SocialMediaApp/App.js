a9dcb04b88faaa6f6a0aea25575e3bfe3445cb17
jwAndroid <cjd9408er@naver.com>
2022-07-26 00:44:47 +0900

    init

---
+++ SocialMediaApp/App.js
@@ -0,0 +1,16 @@
+/**
+ * @format
+ */
+
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const App = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Hello instaApp</Text>
+    </View>
+  );
+};
+
+export default App;
